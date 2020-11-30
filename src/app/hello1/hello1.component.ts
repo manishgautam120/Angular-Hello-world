ad1c56f1aecc6bf8e588382b8660f061788b508e
manishgautam <manish.gautam@redlion.net>
2020-12-01 01:37:26 +0530

    generated hello1 component

---
+++ src/app/hello1/hello1.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello1',
+  templateUrl: './hello1.component.html',
+  styleUrls: ['./hello1.component.css']
+})
+export class Hello1Component {
+
+Country =  "India";
+ 
+
+}
