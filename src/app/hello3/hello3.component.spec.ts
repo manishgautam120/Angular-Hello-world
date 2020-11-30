d8e3a9d84e284d47319f2bdb74c0c9499aaf6b21
manishgautam <manish.gautam@redlion.net>
2020-12-01 03:02:35 +0530

    cretaed component hello3 and changed styleUrlto styles

---
+++ src/app/hello3/hello3.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Hello3Component } from './hello3.component';
+
+describe('Hello3Component', () => {
+  let component: Hello3Component;
+  let fixture: ComponentFixture<Hello3Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Hello3Component ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Hello3Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/hello3/hello3.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello3',
+  templateUrl: './hello3.component.html',
+  styles: ['p{color:yellow}','span{color:red}']
+})
+export class Hello3Component  {
+  styleUrlChnagedToSytles = "i am from hello3"
+}
