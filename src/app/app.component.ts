eaca9499f00656f17c5e12e2db1e564d907bd969
manishgautam <manish.gautam@redlion.net>
2020-12-01 01:38:31 +0530

    app component updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app1';
+  title2 = 'i am from title 2 from app.component.ts';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { Hello1Component } from './hello1/hello1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    Hello1Component
   ],
   imports: [
     BrowserModule
