8c8053dda556b061fa349f2881d24e9854fb80e0
manishgautam <manish.gautam@redlion.net>
2020-12-01 04:23:59 +0530

    added styles

--- src/app/hello8.component.ts
+++ src/app/hello8.component.ts
@@ -5,21 +5,23 @@ import { Component } from '@angular/core';
                 selector:"hello8",
                 template:
                 `
-                 <h6>{{v1}}</h6>
-                 <h6>{{v2}}</h6>
-                 <h6>{{v3}}</h6>
-                 <h6>{{v4}}</h6>
-                 <h6>{{v5}}</h6>
-                `
+                 <h3>{{v1}}</h3>
+                 <h3>{{v2}}</h3>
+                 <h3>{{v3}}</h3>
+                 <h3>{{v4}}</h3>
+                 <h3>{{v5}}</h3>
+                `,
+
+                styles:['h3{color:red}']
 
 
 })
 export class hello8Component{
     //data types
 
-    v1 = "abc"
-    v2 = 'xyz'
+    v1 = "abc";
+    v2 = 'xyz';
     v3 = 20;
-    v4 = 4.5
-
+    v4 = 4.5;
+    v5 = false;
 }
