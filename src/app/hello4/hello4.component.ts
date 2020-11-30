34b9797cb13653f8fe49def3ead217bcb5bcd3ea
manishgautam <manish.gautam@redlion.net>
2020-12-01 03:07:06 +0530

    created component hello4

---
+++ src/app/hello4/hello4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello4',
+  template: 'i am from inline template hello4',
+  styleUrls: ['./hello4.component.css']
+})
+export class Hello4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
