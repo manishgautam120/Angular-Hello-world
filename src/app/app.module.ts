1f45a4023b2af1fe04aff338627e879174474203
manishgautam <manish.gautam@redlion.net>
2020-12-01 03:29:04 +0530

    added hello5.component.ts class name in the app/module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { Hello1Component } from './hello1/hello1.component';
 import { Hello2Component } from './hello2/hello2.component';
 import { Hello3Component } from './hello3/hello3.component';
 import { Hello4Component } from './hello4/hello4.component';
+import { hellp5Component } from './hello5.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { Hello4Component } from './hello4/hello4.component';
     Hello1Component,
     Hello2Component,
     Hello3Component,
-    Hello4Component
+    Hello4Component,
+    hellp5Component
   ],
   imports: [
     BrowserModule
