47dc2e5e62f1708d6348038b4b2635cb1d25b1fe
manishgautam <manish.gautam@redlion.net>
2020-12-01 01:45:32 +0530

    updated app.module.ts for component hello2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { Hello1Component } from './hello1/hello1.component';
+import { Hello2Component } from './hello2/hello2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
-    Hello1Component
+    Hello1Component,
+    Hello2Component
   ],
   imports: [
     BrowserModule
