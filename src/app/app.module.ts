ac98dad3af28ca2a716129edd4de6fa4b7034e48
manishgautam <manish.gautam@redlion.net>
2020-12-01 03:03:54 +0530

    updated file because of component 2 and 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { Hello1Component } from './hello1/hello1.component';
 import { Hello2Component } from './hello2/hello2.component';
+import { Hello3Component } from './hello3/hello3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
     Hello1Component,
-    Hello2Component
+    Hello2Component,
+    Hello3Component
   ],
   imports: [
     BrowserModule
--- src/app/hello2/hello2.component.ts
+++ src/app/hello2/hello2.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hello2',
   templateUrl: './hello2.component.html',
-  styleUrls: ['./hello2.component.css']
+  styleUrls: ['./hello2.component.css','./test.css']
 })
-export class Hello2Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class Hello2Component{
+var1 = 1000;
 
 }
