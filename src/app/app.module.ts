0bf89cd2a261a8d9846099c03a795887ce5e9934
manishgautam <manish.gautam@redlion.net>
2020-12-01 04:42:56 +0530

    added component hello10 in declaration with module in the file src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { Hello4Component } from './hello4/hello4.component';
 import { hellp5Component } from './hello5.component';
 import { hello8Component } from './hello8.component';
 import { hello9Component} from './hello9.component';
+import { hello10Component } from './hello10Component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { hello9Component} from './hello9.component';
     Hello4Component,
     hellp5Component,
     hello8Component,
-    hello9Component
+    hello9Component,
+    hello10Component
   ],
   imports: [
     BrowserModule
