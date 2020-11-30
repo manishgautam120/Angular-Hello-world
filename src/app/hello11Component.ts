f124e6afdb0db98654f9615e5cafdad7e2bcdb0c
manishgautam <manish.gautam@redlion.net>
2020-12-01 04:59:11 +0530

    added matadata in template

--- src/app/hello11Component.ts
+++ src/app/hello11Component.ts
@@ -5,11 +5,11 @@ import { Component } from '@angular/core';
                 selector:"hello11",
                 template:
                 `
-                 <h3>v1 value is : {{v1}}</h3>
-                 <h3>v2 value is :{{v2}}</h3>
-                 <h3>v3 value is :{{v3}}</h3>
-                 <h3>v4 value is :{{v4}}</h3>
-                 <h3>v5 value is :{{v5}}</h3>
+                 <h3>v1 input : ["abc",45,false]; but output : {{v1}}</h3>
+                 <h3>v2 input : ["abc","45","false"]; but output :{{v2}}</h3>
+                 <h3>v3 input : ['abc','45','false']; but output :{{v3}}</h3>
+                 <h3>v4 input : [55,5.5,5.0]; but output :{{v4}}</h3>
+                 <h3>v5 input : [true,false]; but output :{{v5}}</h3>
                 `,
 
                 styles:['h3{color:red}']
